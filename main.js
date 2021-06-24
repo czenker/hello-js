2a5bf7e7c60aef05a13d8561461bc7e3fdbcd055
Christian Zenker <christian.zenker@aoe.com>
2021-06-24 15:51:55 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const app = require('express')()
+const os = require('os')
+require('dotenv').config()
+const yaml = require('js-yaml')
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || "6379",
+});
+
+redisClient.on('error', err => {
+    console.log('Error ' + err);
+});
+
+redisClient.ping(redis.print)
+
+const prettyPlease = (obj) => '<pre>' + yaml.dump(obj) + '</pre>'
+
+app.get('/', (req, res) => res.send('Hello Team Snowball!'))
+
+app.get('/system', (req, res) => res.send(prettyPlease({
+    "user": os.userInfo().username,
+    "hostname": os.hostname(),
+    "arch": os.arch(),
+    "platform": os.platform(),
+    "release": os.release(),
+})))
+
+app.get('/headers', (req, res) => res.send(prettyPlease(req.headers)))
+
+app.get('/env', (req, res) => res.send(prettyPlease(process.env)))
+
+
+app.get('/counter', (req, res) => {
+    redisClient.incr("counter", (err, result) => {
+        err && console.log(err)
+
+        res.send(`${result}`)
+    });
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
